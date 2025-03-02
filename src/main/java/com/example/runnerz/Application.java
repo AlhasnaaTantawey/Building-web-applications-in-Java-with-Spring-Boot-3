2dafcbe6aff8f572e09e5d6633f6abd9d8c40fc9
alhasnaa <alhasnaaelsayed1399@gmail.com>
2025-03-02 13:38:42 +0200

    add second commit

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,13 +1,16 @@
 package com.example.runnerz;
 
+import com.example.runnerz.run.Run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RunnerzApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnerzApplication.class, args);
+		SpringApplication.run(Application.class, args);
+
+
 	}
 
 }
